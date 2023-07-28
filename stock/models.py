b9818b63993fdce7e8f54d63208e5bc041481dad
YUapisit <aps.apisit250@gmail.com>
2023-07-29 01:54:58 +0700

    makeup code

--- stock/models.py
+++ stock/models.py
@@ -7,10 +7,9 @@ STATUS = (
         (2, "customer")
     )
 
-
-
 # ระเบียนข้อมูลคงคลัง
 class ProductData(models.Model):
+    
     product_id = models.AutoField(primary_key=True, unique=True)
     product_code = models.CharField(max_length=16, unique=True, null=True)
     product_name = models.CharField(max_length=256)
@@ -19,24 +18,42 @@ class ProductData(models.Model):
     
     add_date = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        
+        return f"{self.product_code} {self.product_name}"
+    
 class ProductTypeData(models.Model):
+    
     type_id = models.AutoField(primary_key=True, unique=True)
     type_code = models.CharField(max_length=256)
+    type_name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        
+        return f"{self.type_code} {self.type_name}"
     
 class ProductDelete(models.Model):
+    
     product = models.ForeignKey(ProductData, on_delete=models.CASCADE)
     product_code = models.CharField(max_length=16)
     del_unit = models.IntegerField()
     
     del_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        
+        return f"{self.product}"
 
 # ระเบียนข้อมูลกลุ่มผู้ผลิต และลูกค้า
 class AreaData(models.Model):
+    
     area_id = models.AutoField(primary_key=True, unique=True)
     area_name = models.CharField(max_length=128)
+    
+    
    
 class ShopData(models.Model):
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     user_status = models.IntegerField(choices=STATUS)
     shop_id = models.AutoField(primary_key=True, unique=True)
@@ -57,7 +74,13 @@ class ShopData(models.Model):
     
     add_date = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        
+        return f"{self.shop_code} {self.shop_name}"
+
+
 class CustomerData(models.Model):
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     user_status = models.IntegerField(choices=STATUS)
     customer_id = models.AutoField(primary_key=True, unique=True)
@@ -74,11 +97,16 @@ class CustomerData(models.Model):
     customer_remark = models.TextField(default="-")
     
     add_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        
+        return self.customer_code+" "+self.customer_name
  
- 
+
 # ระเบียนรายการรับสินค้าเข้า และจ่ายสินค้าออก 
 # เอกสารการรับสินค้า
 class InputInvoice(models.Model):
+    
     invoice_id = models.AutoField(primary_key=True, unique=True)
     invoice_no = models.CharField(max_length=16, unique=True, null=True)
     shop = models.ForeignKey(ShopData, on_delete=models.CASCADE)
@@ -88,8 +116,13 @@ class InputInvoice(models.Model):
     
     input_date = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        
+        return self.invoice_no
+    
 # การรับสินค้า
 class InputData(models.Model):
+    
     invoice = models.ForeignKey(InputInvoice, on_delete=models.CASCADE)
     invoice_no = models.CharField(max_length=16,  null=True)
     product = models.ForeignKey(ProductData, on_delete=models.CASCADE)
@@ -97,8 +130,13 @@ class InputData(models.Model):
     unit_price = models.FloatField()
     discount = models.FloatField()
     
+    def __str__(self):
+    
+        return self.invoice_no
+    
 # เอกสารการจ่าสินค้าออก
 class OutputInvoice(models.Model):
+    
     invoice_id = models.AutoField(primary_key=True, unique=True)
     invoice_no = models.CharField(max_length=16, unique=True, null=True)
     customer = models.ForeignKey(CustomerData, on_delete=models.CASCADE)
@@ -107,28 +145,40 @@ class OutputInvoice(models.Model):
     remark = models.TextField(default="-")
     
     input_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+    
+        return self.invoice_no
 
 # การจ่าสินค้าออก
 class OutputData(models.Model):
+    
     invoice = models.ForeignKey(OutputInvoice, on_delete=models.CASCADE)
     invoice_no = models.CharField(max_length=16,  null=True)
     product = models.ForeignKey(ProductData, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     sale_price = models.FloatField()
     discount = models.FloatField()
+    
+    def __str__(self):
+        
+        return self.invoice_no
       
     
 class DBCounter(models.Model):
+    
     counter_type = models.CharField(max_length=16, primary_key=True, unique=True)
     counter_number = models.IntegerField()
  
 class DBDateCounter(models.Model):
+    
     counter_type = models.ForeignKey(DBCounter, on_delete=models.CASCADE)
     counter_number = models.IntegerField()
     counter_date = models.DateTimeField(auto_now_add=True)
     
     
 class TempInputDB(models.Model):
+    
     invoice = models.CharField(max_length=16)
     invoice_no = models.CharField(max_length=16)
     product = models.CharField(max_length=16)
@@ -136,10 +186,19 @@ class TempInputDB(models.Model):
     unit_price = models.FloatField()
     discount = models.FloatField()
     
+    def __str__(self):
+        
+        return self.invoice_no
+    
 class TempOutputDB(models.Model):
+    
     invoice = models.CharField(max_length=16)
     invoice_no = models.CharField(max_length=16)
     product = models.CharField(max_length=16)
     quantity = models.IntegerField()
     sale_price = models.FloatField()
     discount = models.FloatField()
+
+    def __str__(self):
+            
+            return self.invoice_no
--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 import json
 import pytz
 from django.shortcuts import get_object_or_404
@@ -15,20 +13,23 @@ from django.contrib.auth import authenticate, login
 from django.db.models import Q
 from django.views.decorators.http import require_POST
 
-
 from . import models
 from . import serializers
+
+# Create your views here.
 
 @csrf_exempt
 @api_view(["GET", ])
 @permission_classes((AllowAny,))
 def get_user(request):
+
     user_id = request.session['_auth_user_id']
     user_status = User.objects.get(id=user_id)
+
     return Response(
         {
-            "status":True,
-            "uid":user_id
+            "status": True,
+            "uid": user_id
         }
     )
 
@@ -39,17 +40,20 @@ def get_user(request):
 def getProvince(request):
     data = {}
     msg = 'already get thai province'
-    
 
-    # get thai province file
     result = finders.find('data/json/thai_province.json')
     with open(result, encoding='utf-8') as f:
         province = json.load(f)
 
     data['province'] = province.keys()
 
-    return Response({'status': True, 'message': msg, 'data': data['province']})
-
+    return Response(
+        {
+            'status': True,
+            'message': msg,
+            'data': data['province'],
+        }
+    )
 
 
 @csrf_exempt
@@ -60,7 +64,6 @@ def getDistrict(request):
     msg = 'already get thai district'
     print(request.data['province'])
 
-    # get thai province file
     result = finders.find('data/json/thai_province.json')
     with open(result, encoding='utf-8') as f:
         province = json.load(f)
@@ -68,8 +71,14 @@ def getDistrict(request):
     province_selected = request.data['province']
     data['district'] = province[province_selected].keys()
 
-    return Response({'status': True, 'message': msg, 'data': data['district']})
+    return Response(
+        {
+            'status': True,
+            'message': msg,
+            'data': data['district'],
+        }
 
+    )
 
 
 @csrf_exempt
@@ -78,7 +87,7 @@ def getDistrict(request):
 def getTambon(request):
     data = {}
     msg = 'already get thai tambon'
-    
+
     # get thai province file
     result = finders.find('data/json/thai_province.json')
     with open(result, encoding='utf-8') as f:
@@ -89,8 +98,13 @@ def getTambon(request):
 
     data['tambon'] = province[province_selected][district_selected]
 
-    return Response({'status': True, 'message': msg, 'data': data['tambon']})
-
+    return Response(
+        {
+            'status': True,
+            'message': msg,
+            'data': data['tambon']
+        }
+    )
 
 
 @require_POST
@@ -104,13 +118,13 @@ def createInputData(request):
     user = User.objects.get(id=user_id)
     object_product = json.loads(obj_string)
     products = []
-    
+
     total_price = 0
     discount = 0
-    
+
     status = True
     
-    for i , o in dict(object_product).items():
+    for i, o in dict(object_product).items():
         pro = {}
         for x, y in o.items():
             pro[x] = y
@@ -118,87 +132,97 @@ def createInputData(request):
                 total_price += y
             if x == 'discount':
                 discount += y
-        products.append(pro) 
-            
+        products.append(pro)
+
     print(products)
     print(total_price, discount)
-    
-    try :
+
+    try:
         shop = models.ShopData.objects.get(user=user)
-    
+
         if models.InputInvoice.objects.all().count() == 0:
+
             inv_id = 100001
             invoice = models.InputInvoice.objects.create(
-                invoice_id = inv_id,
+                invoice_id=inv_id,
                 shop=shop,
                 remark=remark,
                 total_price=total_price,
                 discount=discount,
             )
-        else :
+
+        else:
+
             invoice = models.InputInvoice.objects.create(
                 shop=shop,
                 remark=remark,
                 total_price=total_price,
                 discount=discount,
             )
-        
-        models.InputInvoice.objects.filter(invoice_id=invoice.invoice_id).update(invoice_no=f"I{shop.shop_code}{invoice.invoice_id}")
-        
+
+        models.InputInvoice.objects.filter(invoice_id=invoice.invoice_id).update(
+            invoice_no=f"I{shop.shop_code}{invoice.invoice_id}"
+        )
+
         for product in products:
             product_name = product['product_name']
             product_desc = product['product_detail']
             product_cost = product['price']
             product_unit = product['unit']
             product_disc = product['discount']
-            
+
             _product = models.ProductData.objects
-            
+
             if models.ProductData.objects.all().count() == 0:
-                
-                _product= models.ProductData.objects.create(
-                    product_id = 100000,
-                    product_name = product_name,
-                    unit_cost = product_cost,
-                    product_desc = product_desc
+
+                _product = models.ProductData.objects.create(
+                    product_id=100000,
+                    product_name=product_name,
+                    unit_cost=product_cost,
+                    product_desc=product_desc
                 )
-            else :
-                _product= models.ProductData.objects.create(
-                    product_name = product_name,
-                    unit_cost = product_cost,
-                    product_desc = product_desc             
+            else:
+                _product = models.ProductData.objects.create(
+                    product_name=product_name,
+                    unit_cost=product_cost,
+                    product_desc=product_desc
                 )
-                
-            models.ProductData.objects.filter(product_id=_product.product_id).update(product_code=f"P{_product.product_id}")
-                
-            print(f"No. {models.InputInvoice.objects.get(invoice_id=invoice.invoice_id).invoice_no}")
-                    
+
+            models.ProductData.objects.filter(product_id=_product.product_id).update(
+                product_code=f"P{_product.product_id}")
+
+            print(
+                f"No. {models.InputInvoice.objects.get(invoice_id=invoice.invoice_id).invoice_no}"
+            )
+
             models.InputData.objects.create(
-                invoice = invoice,
-                invoice_no = models.InputInvoice.objects.get(invoice_id=invoice.invoice_id).invoice_no,
-                product = _product,
-                quantity = product_unit,
+                invoice=invoice,
+                invoice_no=models.InputInvoice.objects.get(
+                    invoice_id=invoice.invoice_id).invoice_no,
+                product=_product,
+                quantity=product_unit,
                 discount=product_disc,
                 unit_price=product_cost
-                
+
+            )
+
+            models.TempInputDB.objects.create(
+                invoice=invoice.invoice_id,
+                invoice_no=models.InputInvoice.objects.get(
+                    invoice_id=invoice.invoice_id).invoice_no,
+                product=models.ProductData.objects.get(
+                    product_id=_product.product_id).product_code,
+                quantity=product_unit,
+                discount=product_disc,
+                unit_price=product_cost
             )
             
-            models.TempInputDB.objects.create(
-                invoice = invoice.invoice_id,
-                invoice_no = models.InputInvoice.objects.get(invoice_id=invoice.invoice_id).invoice_no,
-                product = models.ProductData.objects.get(product_id=_product.product_id).product_code,
-                quantity = product_unit,
-                discount=product_disc,
-                unit_price=product_cost
-                
-            )
     except TypeError as err:
-        
         print(err)
         status = False
 
     return Response({
-        "status":status
+        "status": status
     })
 
 
@@ -207,7 +231,7 @@ def createInputData(request):
 @permission_classes((AllowAny,))
 def createShop(request):
     data = request.data
-    
+
     status = True
     _username = data['username']
     _password = data['password']
@@ -223,87 +247,87 @@ def createShop(request):
     _fax = data['fax']
     _email = data['email']
     _remark = data['remark']
-    
+
     if User.objects.filter(username=_username).count() != 0:
-        return Response({"status":False, "message":'มีชื่อผู้ใช้นี้แล้ว'})
-    
-    try :
+        return Response({"status": False, "message": 'มีชื่อผู้ใช้นี้แล้ว'})
+
+    try:
         _user = User.objects.create_user(
             email=_email,
             username=_username,
             password=_password
         )
-        if _user :
-            
+        if _user:
+
             if models.ShopData.objects.all().count() == 0:
                 shop_id = 10000
-                
+
                 shop = models.ShopData.objects.create(
-                user = _user,
-                user_status = 1,
-                shop_id=shop_id,
-                shop_name=_shop_name,
-                shop_product_type=_product_type,
-                shop_contact = _contact,
-                shop_post_code = _post_id,
-                shop_province = _province,
-                shop_district=_district,
-                shop_subdistrict=_subDistrict,
-                shop_detail_address=_detail_address,
-                shop_tel=_tel,
-                shop_fax = _fax,
-                shop_email=_email,
-                shop_remark = _remark
-                )
-                
-            else :
-            
-                shop = models.ShopData.objects.create(
-                    user = _user,
-                    user_status = 1,
+                    user=_user,
+                    user_status=1,
+                    shop_id=shop_id,
                     shop_name=_shop_name,
                     shop_product_type=_product_type,
-                    shop_contact = _contact,
-                    shop_post_code = _post_id,
-                    shop_province = _province,
+                    shop_contact=_contact,
+                    shop_post_code=_post_id,
+                    shop_province=_province,
                     shop_district=_district,
                     shop_subdistrict=_subDistrict,
                     shop_detail_address=_detail_address,
                     shop_tel=_tel,
-                    shop_fax = _fax,
-                    shop_email=_email
+                    shop_fax=_fax,
+                    shop_email=_email,
+                    shop_remark=_remark
+                )
+
+            else:
+
+                shop = models.ShopData.objects.create(
+                    user=_user,
+                    user_status=1,
+                    shop_name=_shop_name,
+                    shop_product_type=_product_type,
+                    shop_contact=_contact,
+                    shop_post_code=_post_id,
+                    shop_province=_province,
+                    shop_district=_district,
+                    shop_subdistrict=_subDistrict,
+                    shop_detail_address=_detail_address,
+                    shop_tel=_tel,
+                    shop_fax=_fax,
+                    shop_email=_email,
                     # shop_remark = _remark
                 )
-                
-            models.ShopData.objects.filter(shop_id=shop.shop_id).update(shop_code=f"S{shop.shop_id}")
- 
+
+            models.ShopData.objects.filter(shop_id=shop.shop_id).update(
+                shop_code=f"S{shop.shop_id}")
+
     except TypeError as err:
-        
+
         status = False
         print(err)
 
     return Response(
         {
-            "status":status,
-            "message":"success"
+            "status": status,
+            "message": "success"
         }
     )
-
 
 
 @csrf_exempt
 @api_view(["POST",])
 @permission_classes((AllowAny,))
 def login_api(request):
-    
+
     username = request.data['username']
     password = request.data['password']
-    
+
     user = authenticate(
         username=username,
         password=password
     )
-    
+
     if user is not None:
         if user.is_active:
             status = True
@@ -318,86 +342,93 @@ def login_api(request):
 
     return Response({'status': status, 'message': msg})
 
+
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes((AllowAny,))
 def getAllProduct(request):
     product_data = models.ProductData.objects.all()
     products = serializers.ProductDataSerializer(product_data, many=True).data
-    
+
     return Response(
         {
-            "status":True,
-            "data":products
-            
+            "status": True,
+            "data": products
+
         }
     )
+
 
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes((AllowAny,))
 def getAllShop(request):
     status = True
-    
+
     shop_data = models.ShopData.objects.all()
     shop = serializers.ShopDataSerializer(shop_data, many=True).data
-    
+
     return Response(
         {
-            "status":status,
-            "data":shop
+            "status": status,
+            "data": shop
         }
     )
 
 
 @csrf_exempt
 @api_view(["POST"])
-@permission_classes((AllowAny,))  
+@permission_classes((AllowAny,))
 def editProduct(request):
-    
+
     edit = request.data
     status = True
     product_id = edit['product_id']
-    product_name = edit['product_name']    
+    product_name = edit['product_name']
     product_desc = edit['product_desc']
     unit_cost = edit['unit_cost']
-    
+
     print(product_name)
-    
-    try :
+
+    try:
         models.ProductData.objects.filter(product_id=product_id).update(
             product_name=product_name,
             product_desc=product_desc,
             unit_cost=unit_cost
         )
-        
-    except TypeError as err :
+
+    except TypeError as err:
         print(err)
         status = False
-    
-    
-    return Response({
-        "status":status
-    })
-    
+
+    return Response(
+        {
+            "status": status
+        }
+    )
+
+
 @csrf_exempt
 @api_view(["POST"])
-@permission_classes((AllowAny,))  
+@permission_classes((AllowAny,))
 def deleteProduct(request):
     id = request.data['product_code']
     models.ProductData.objects.filter(product_code=id).delete()
-    
-    return Response({
-        "status":True
-    })
+
+    return Response(
+        {
+            "status": True
+        }
+    )
 
 
 @csrf_exempt
 @api_view(["POST"])
-@permission_classes((AllowAny,))  
+@permission_classes((AllowAny,))
 def editShop(request):
+
     status = True
-    
+
     shop_id = request.data['shop_id']
     shop_name = request.data['shop_name']
     shop_product_type = request.data['shop_product_type']
@@ -411,44 +442,44 @@ def editShop(request):
     shop_fax = request.data['shop_fax']
     shop_email = request.data['shop_email']
     shop_remark = request.data['shop_remark']
-    
-    try :
+
+    try:
         models.ShopData.objects.filter(shop_id=shop_id).update(
-            shop_name = shop_name,
-            shop_product_type = shop_product_type,
-            shop_contact = shop_contact,
-            shop_province = shop_province,
-            shop_district = shop_district,
-            shop_subdistrict = shop_subdistrict,
-            shop_detail_address = shop_detail_address,
-            shop_post_code = shop_post_code,
-            shop_tel = shop_tel,
-            shop_fax = shop_fax,
-            shop_email = shop_email,
-            shop_remark = shop_remark
+            shop_name=shop_name,
+            shop_product_type=shop_product_type,
+            shop_contact=shop_contact,
+            shop_province=shop_province,
+            shop_district=shop_district,
+            shop_subdistrict=shop_subdistrict,
+            shop_detail_address=shop_detail_address,
+            shop_post_code=shop_post_code,
+            shop_tel=shop_tel,
+            shop_fax=shop_fax,
+            shop_email=shop_email,
+            shop_remark=shop_remark
         )
-    
+
     except TypeError as err:
         print(err)
         status = False
-    
-    
-        
-    
+
     return Response({
-        "status":status
+        "status": status
     })
-    
+
+
 @csrf_exempt
 @api_view(["POST"])
-@permission_classes((AllowAny,))  
+@permission_classes((AllowAny,))
 def deleteShop(request):
     id = request.data['shop_id']
     print(models.ShopData.objects.get(shop_id=id).user)
-    
-    User.objects.filter(username=models.ShopData.objects.get(shop_id=id).user).delete()
-    
-    return Response({
-        "status":True
-    })
 
+    User.objects.filter(
+        username=models.ShopData.objects.get(shop_id=id).user).delete()
+
+    return Response(
+        {
+            "status": True
+        }
+    )
